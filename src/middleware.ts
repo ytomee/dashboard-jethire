3d961f241b7b0e51614332e0dacdaba3d75fb339
Tomé Almeida <121977238+ytomee@users.noreply.github.com>
2025-05-03 19:41:23 +0100

    middleware working

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,26 +6,17 @@ import { getToken } from 'next-auth/jwt';
 const PUBLIC_PATHS = [
   '/signin',
   '/signup',
-  '/public',
-  '/lib',
-  '/api',
-  '/_next',
-  '/favicon.ico',
-  '/images'
 ];
 
 export async function middleware(req: NextRequest) {
   
-  
   const { pathname } = req.nextUrl;
-  
   
   const isPublic = PUBLIC_PATHS.some((path) =>
     pathname === path || pathname.startsWith(path + '/')
   );
   if (isPublic) return NextResponse.next();
 
-  
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   
   if (!token) {
