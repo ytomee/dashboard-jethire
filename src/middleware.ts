861587a1b88cfc881bbc747a0020523ecb04a708
Tomé Almeida <121977238+ytomee@users.noreply.github.com>
2025-05-03 16:21:38 +0100

    middleware fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-//import { getToken } from 'next-auth/jwt';
+import { getToken } from 'next-auth/jwt';
 
 const PUBLIC_PATHS = [
   '/signin',
@@ -25,16 +25,15 @@ export async function middleware(req: NextRequest) {
   );
   if (isPublic) return NextResponse.next();
 
-  /*
+  
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-
+  
   if (!token) {
     const signInUrl = new URL('/signin', req.url);
     return NextResponse.redirect(signInUrl);
+  } else {
+    return NextResponse.next();
   }
-  */
-
-  return NextResponse.next();
 }
 
 export const config = {
