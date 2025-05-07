09c1c4b920cef0ab716ef416b7759d7bfc7e49cc
Tomé Almeida <121977238+ytomee@users.noreply.github.com>
2025-05-07 22:40:09 +0100

    update duplicate company

--- src/app/api/company/register-company/route.ts
+++ src/app/api/company/register-company/route.ts
@@ -40,7 +40,7 @@ export async function POST(req: NextRequest) {
       let adminCounter = 1;
 
       while (await Company.exists({ "team.user": username })) {
-        username = `${baseAdminUsername}${adminCounter}`;
+        username = `${baseCompanyUsername}-${baseAdminUsername}${adminCounter}`;
         adminCounter++;
       }
 
