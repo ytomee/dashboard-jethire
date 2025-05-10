12de95565be342634291f3f5ffff4187d9a95628
Tomé Almeida <121977238+ytomee@users.noreply.github.com>
2025-05-10 16:34:39 +0100

    deploy fixed

--- src/app/api/company/list/route.ts
+++ src/app/api/company/list/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import Company from "@/models/company";
 import dbConnect from "@/lib/dbConnect";
 
